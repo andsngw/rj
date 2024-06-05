ba1e8d1122cfcb7fe4532fe46ee3454276ef466d
andsngw <ch10sprta28ti@gmail.com>
2024-06-05 16:03:45 +0900

    ex1

--- pose.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("yolov8x-pose.pt")
-results = model("https://ultralytics.com/images/bus.jpg", save=True,
-save_txt=True, save_conf=True)
-keypoints = results[0].keypoints
-print(keypoints.data)
