6a362b92824ec65b6918d976751967273ea7f19f
andsngw <ch10sprta28ti@gmail.com>
2024-06-12 16:54:04 +0900

    ex2

--- ex2.py
+++ ex2.py
@@ -5,13 +5,10 @@ import numpy as np
 # YOLOモデルの読み込み
 model = YOLO("yolov8x-pose.pt")
 
-
-
 # 画像を読み込む
 images = ["ex1.jpg", "ex2_307.jpg", "ex2_336.jpg", "ex2_2015.jpg", "ex2_3077.jpg", "ex2_5175.jpg"] 
 keypoints_list = {} #それぞれの画像におけるx,y座標のリスト
-sum = {}
-
+sum_distances = {}  # 画像間の距離を格納する辞書
 
 for image in images:
     results = model(image, save=True, save_txt=True, save_conf=True)
@@ -21,18 +18,16 @@ for image in images:
     path = image
     img = cv2.imread(path)
 
+
 # 画像ごとにキーポイントの座標の差分を計算
-sum_dif_x = {}
-sum_dif_y = {}
 base_image = images[0]
 
 for image in images:
     dif = keypoints_list[base_image] - keypoints_list[image]
-    sum_dif_x[image] = abs(dif[:, 0]).sum()
-    sum_dif_y[image] = abs(dif[:, 1]).sum()
+    dis = np.linalg.norm(dif)
+    sum_distances[image] = dis
 
-    sum[image] = pow((pow(sum_dif_x[image], 2) + pow(sum_dif_y[image], 2)), 0.5)
-
-sorted_images = sorted(sum, key=sum.get)
+# 距離が小さい順に画像を並び替えてプリント
+sorted_images = sorted(sum_distances, key=sum_distances.get)
 for image in sorted_images:
     print(f"Image: {image}")
